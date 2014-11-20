f247462b20823b800c5e8ff9350a9c8f97205ea8
spoiledtechie <spoiledtechie@gmail.com>
2014-11-20 18:52:53 -0500

    navigation at the top.

--- RDN.League/App_Start/BundleConfig.cs
+++ RDN.League/App_Start/BundleConfig.cs
@@ -14,19 +14,29 @@ namespace RDN.League
                         "~/Scripts/Knockout-{version}.js",
                         "~/Scripts/Knockout.mapping-{version}.js",
                         "~/Scripts/jquery/jquery.fileupload.js",
-                        "~/Scripts/jquery/jquery.numeric.js"
-
+                        "~/Scripts/jquery/jquery.numeric.js",
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js",
+                        "~/Scripts/knockout.validation.js",
+                        "~/Scripts/jquery.idTabs.min.js",
+                        "~/Scripts/jquery.dataTables.min.js",
+                        "~/Scripts/Pages/AddOldGame.js",
+                        "~/Scripts/jquery/jquery-ui-1.10.4.custom.min.js",
+                        "~/Scripts/jquery/jquery.ui.timepicker.addon.js",
+                        "~/Scripts/jquery.timer.js",
+                        "~/Scripts/jquery.tokeninput.js",
+                        "~/Scripts/jquery.dataTables.naturalSort.js",
+                        "~/Scripts/colorPicker/colorpicker.js",
+                        "~/Scripts/jquery.MultiFile.js",
+                        "~/Scripts/jquery/jquery.ajaxfileupload.js",
+                        "~/Scripts/tinymce/tiny_mce.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/viewmodels").Include(
-                        "~/Scripts/main.js",
-                        "~/Scripts/ViewModels/Payments.js",
-                        "~/Scripts/ViewModels/AdModel.js",
-                        "~/Scripts/ViewModels/PropertiesModel.js",
-                        "~/Scripts/ViewModels/BuyModel.js",
-                        "~/Scripts/ViewModels/SubscriptionsModel.js",
-                        "~/Scripts/ViewModels/ShoppingCartModel.js",
-                        "~/Scripts/ViewModels/ReceiptModel.js"));
+                "~/Scripts/ViewModels/LeaguesViewModel.js",
+                        "~/Scripts/ViewModels/MembersViewModel.js",        
+                "~/Scripts/main.js"
+                        ));
 
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -44,9 +54,11 @@ namespace RDN.League
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/jquery-ui.min.css",
                       "~/Content/font-awesome.css",
-                      "~/Content/site.css"));
+                      "~/Content/main.css",
+                      "~/Content/jquery-ui-1.10.4.custom.css",
+                      "~/Content/FullCalendar/fullcalendar.css",
+                      "~/Content/FullCalendar/fullcalendar.print.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
