96d864884d907fc323fc4dd58b2229391a3acdd0
arpitjain240188 <arpit.jain240188@gmail.com>
2015-04-25 23:35:48 +0530

    Bug ID:  25495 (Export Emergency Contacts) fix

---
+++ RDN.Library/Classes/League/Reports/MembersReportContactEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RDN.Library.Classes.League.Reports
+{
+    public enum MembersReportContactEnum
+    {
+        Member_First_Name = 1,
+        Member_Last_Name = 2,
+        First_Name = 3,
+        Last_Name = 4,
+        Email = 5,
+        Phone_Number = 6,
+        Address_Line_1 = 7,
+        Address_Line_2 = 8,
+        City = 9,
+        State = 10,
+        Zip = 11,
+        Country = 12
+    }
+}
