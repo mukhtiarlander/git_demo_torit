a97f66b1b20cad81ca3b8f07f3862ac2b327834b
spoiledtechie <spoiledtechie@gmail.com>
2015-04-25 00:10:36 -0400

    adding config files.

---
+++ RDN.Portable/Classes/Url/LeagueUrls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RDN.Portable.Classes.Url
+{
+    public static class LeagueUrls
+    {
+        public const string ClearExchangeRates = "/utilities/ClearCurrencyExchangeRates";
+
+
+    }
+}
---
+++ RDN.Portable/Classes/Url/PublicUrls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RDN.Portable.Classes.Url
+{
+    public static class PublicUrls
+    {
+        public const string ScoreboardUrl = "/roller-derby-scoreboard";
+    }
+}
