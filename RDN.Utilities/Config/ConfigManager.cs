53a1338c6cff9139242b9b7c702ba9fdf66167e5
spoiledtechie <spoiledtechie@gmail.com>
2015-05-04 01:09:00 -0400

    added the config manager back.

---
+++ RDN.Utilities/Config/ConfigManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RDN.Utilities.Config
+{
+    public class ConfigManager
+    {
+        private static string Get(string key)
+        {
+            return GetConfigString(key);
+        }
+
+        private static string GetConfigString(string key)
+        {
+            var cleanedKey = key.Replace("get_", "");
+            return System.Configuration.ConfigurationManager.AppSettings[cleanedKey];
+        }
+
+        public static string WebsiteShortName
+        {
+            get { return Get("WebsiteShortName"); }
+        }
+        public static string PrivacyUrl
+        {
+            get { return Get("PrivacyUrl"); }
+        }
+        public static string InternalSite
+        {
+            get { return Get("InternalSite"); }
+        }
+
+        public static string NameOfMembers
+        {
+            get { return Get("NameOfMembers"); }
+        }
+    }
+}
