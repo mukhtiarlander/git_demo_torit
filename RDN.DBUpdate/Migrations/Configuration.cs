7a22f198050e3d5b53916f73c101d402b95ee3dd
arpitjain240188 <arpit.jain240188@gmail.com>
2015-05-12 01:01:06 +0530

    Email Server

--- RDN.DBUpdate/Migrations/Configuration.cs
+++ RDN.DBUpdate/Migrations/Configuration.cs
@@ -24,7 +24,7 @@ namespace RDN.DBUpdate.Migrations
     //{
     //    public CRN()
     //    {
-    //        AutomaticMigrationsEnabled = true;
+    //        AutomaticMigrationsEnabled = true;    
     //        AutomaticMigrationDataLossAllowed = true;
     //    }
 
