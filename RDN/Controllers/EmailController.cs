99d5da074deb8b9403a8de038b73ca22365ba27f
Scott <spoiledtechie@gmail.com>
2015-11-24 17:01:37 -0500

    email unsubscribe service

---
+++ RDN/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using RDN.Library.Classes.EmailServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RDN.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult UnSubscribe(string email, string listType)
+        {
+            var typeList = (SubscriptionServiceType)Enum.Parse(typeof(SubscriptionServiceType), listType);
+
+            if (typeList == SubscriptionServiceType.subscription)
+            {
+                ViewBag.Successful = EmailServer.AddEmailToUnsubscribeList(email);
+
+            }
+
+
+
+            return View();
+        }
+    }
+}
