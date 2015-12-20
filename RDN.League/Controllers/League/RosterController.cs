a5bfce6b8d3e4d6cde04e3debab725561d4c9102
dharmesh-parekh <er.dharmeshparekh@gmail.com>
2015-12-20 12:58:01 +0530

    RDN-1191 Add Roster Selections for Games

---
+++ RDN.League/Controllers/League/RosterController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RDN.League.Models.Helpers;
+using RDN.League.Models.Roster;
+using RDN.Library.Cache;
+using RDN.Library.Classes.Roster;
+using RDN.Library.Util;
+using RDN.Library.Util.Enum;
+using RDN.Portable.Classes.Federation.Enums;
+using RDN.Portable.Classes.Insurance;
+
+namespace RDN.League.Controllers.League
+{
+    public class RosterController : BaseController
+    {
+        // GET: Roster
+        [Authorize]
+        public ActionResult ViewRosters()
+        {
+            NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(Request.Url.Query);
+
+            string updated = nameValueCollection["u"];
+
+            if (!String.IsNullOrEmpty(updated) && updated == SiteMessagesEnum.s.ToString())
+            {
+                SiteMessage message = new SiteMessage();
+                message.MessageType = SiteMessageType.Info;
+                message.Message = "Roster information updated.";
+                this.AddMessage(message);
+
+            }
+            if (!String.IsNullOrEmpty(updated) && updated == SiteMessagesEnum.sac.ToString())
+            {
+                SiteMessage message = new SiteMessage();
+                message.MessageType = SiteMessageType.Info;
+                message.Message = "New Roster Successfully Added.";
+                this.AddMessage(message);
+            }
+            if (!String.IsNullOrEmpty(updated) && updated == SiteMessagesEnum.de.ToString())
+            {
+                SiteMessage message = new SiteMessage();
+                message.MessageType = SiteMessageType.Info;
+                message.Message = "Successfully Deleted.";
+                this.AddMessage(message);
+            }
+            var memId = RDN.Library.Classes.Account.User.GetMemberId();
+            var league = MemberCache.GetLeagueOfMember(memId);
+            var lst = RosterManager.GetLeagueRosters(league.LeagueId);
+
+            return View(lst);
+        }
+
+        [Authorize]
+        public ActionResult ViewRoster(long id, string leagueId)
+        {
+            var memId = RDN.Library.Classes.Account.User.GetMemberId();
+            var league = MemberCache.GetLeagueOfMember(memId);
+            var roster = RosterManager.GetLeagueRoster(id, league.LeagueId);
+            return View(roster);
+        }
+        [Authorize]
+        public ActionResult AddNewRoster()
+        {
+            var memId = RDN.Library.Classes.Account.User.GetUserId();
+            var model = new RosterModel();
+            var members = MemberCache.GetCurrentLeagueMembers(memId).Select(x => new KeyValueHelper()
+            {
+                Id = x.MemberId,
+                Name = x.Name
+            }).ToList();
+            model.Members = members;
+            model.InsuranceTypes = new SelectList(EnumerationHelper.GetAll<InsuranceType>(), "Key", "Value");
+            model.RuleSets = new SelectList(EnumerationHelper.GetAll<RuleSetsUsedEnum>(), "Key", "Value");
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult AddNewRoster(RosterModel model)
+        {
+            var roster = new Roster();
+            roster.RosterId = model.RosterId;
+            roster.RosterName = model.RosterName;
+            roster.RosterMemberIds = model.RosterMemberIds;
+            roster.GameDate = model.GameDate;
+            roster.RosterMembers = model.RosterMembers;
+            roster.InsuranceTypeId = model.InsuranceTypeId;
+            roster.RuleSetsUsedEnum = model.RuleSetsUsedEnum;
+            var memId = RDN.Library.Classes.Account.User.GetMemberId();
+            var league = MemberCache.GetLeagueOfMember(memId);
+            roster.LeagueId = league.LeagueId;
+            bool isexecute =  RosterManager.CreateNewRoster(roster);
+            if(isexecute)
+                return Redirect(Url.Content("~/league/rosters/all?u=" + SiteMessagesEnum.sac));
+
+            return View(model);
+        }
+
+        [Authorize]
+        public ActionResult EditRoster(long id, string leagueId)
+        {
+            var memId = RDN.Library.Classes.Account.User.GetMemberId();
+
+            var roster = RDN.Library.Classes.Roster.RosterManager.GetLeagueRoster(id, new Guid(leagueId));
+            var model = new RosterModel();
+            model.RosterId = roster.RosterId;
+            model.RosterName = roster.RosterName;
+            model.GameDate = roster.GameDate;
+            model.RosterMembers = roster.RosterMembers;
+            model.InsuranceTypeId = roster.InsuranceTypeId;
+            model.RuleSetsUsedEnum = roster.RuleSetsUsedEnum;
+            var members = MemberCache.GetCurrentLeagueMembers(memId).Select(x => new KeyValueHelper()
+            {
+                Id = x.MemberId,
+                Name = x.Name
+            }).ToList();
+            model.Members = members;
+            model.RosterMemberIds = string.Join(",", roster.RosterMembers.Select(x => x.Id).ToArray());
+            model.InsuranceTypes = new SelectList(EnumerationHelper.GetAll<InsuranceType>(), "Key", "Value");
+            model.RuleSets = new SelectList(EnumerationHelper.GetAll<RuleSetsUsedEnum>(), "Key", "Value");
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult EditRoster(RosterModel model)
+        {
+            var roster = new Roster();
+            roster.RosterId = model.RosterId;
+            roster.RosterName = model.RosterName;
+            roster.RosterMemberIds = model.RosterMemberIds;
+            roster.GameDate = model.GameDate;
+            roster.RosterMembers = model.RosterMembers;
+            roster.InsuranceTypeId = model.InsuranceTypeId;
+            roster.RuleSetsUsedEnum = model.RuleSetsUsedEnum;
+            var memId = RDN.Library.Classes.Account.User.GetMemberId();
+            var league = MemberCache.GetLeagueOfMember(memId);
+            roster.LeagueId = league.LeagueId;
+            bool isexecute = RosterManager.UpdateRoster(roster);
+            if (isexecute)
+                return Redirect(Url.Content("~/league/rosters/all?u=" + SiteMessagesEnum.sac));
+
+            return View(model);
+        }
+
+    }
+}
---
+++ RDN.Library/Classes/Roster/RosterManager.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Site.Classes.Exception;
+using Geocoding;
+using RDN.Library.Cache;
+using RDN.Library.Classes.League.Links;
+using RDN.Library.DataModels.Context;
+using RDN.Library.DataModels.Roster;
+using RDN.Library.Util;
+
+namespace RDN.Library.Classes.Roster
+{
+    public class RosterManager
+    {
+        public static bool CreateNewRoster(Roster roster)
+        {
+            try
+            {
+                var dc = new ManagementContext();
+                DataModels.Roster.Roster rosterObj = new DataModels.Roster.Roster();
+                rosterObj.RosterName = roster.RosterName;
+                rosterObj.GameDate = roster.GameDate;
+                rosterObj.League = dc.Leagues.FirstOrDefault(x => x.LeagueId == roster.LeagueId);
+                rosterObj.RuleSetsUsedEnum = roster.RuleSetsUsedEnum;
+                if (!string.IsNullOrEmpty(roster.RosterMemberIds))
+                {
+                    var rosterMembers = roster.RosterMemberIds.Split(',');
+                    foreach (var memberId in rosterMembers)
+                    {
+                        var rosterMember = new RosterMember();
+                        rosterMember.Member = dc.Members.FirstOrDefault(x => x.MemberId == new Guid(memberId));
+                        rosterMember.Roster = rosterObj;
+                        rosterMember.InsuranceType = roster.InsuranceTypeId;
+                        rosterObj.RosterMembers.Add(rosterMember);
+                    }
+                }
+                dc.Rosters.Add(rosterObj);
+                int c = dc.SaveChanges();
+                return c > 0;
+
+            }
+            catch (Exception exception)
+            {
+                ErrorDatabaseManager.AddException(exception, exception.GetType());
+
+            }
+            return false;
+        }
+
+        public static List<Roster> GetLeagueRosters(Guid leagueId)
+        {
+            List<Roster> itemRosters = new List<Roster>();
+            try
+            {
+                var dc = new ManagementContext();
+
+                var rosters = dc.Rosters.Where(x => x.League.LeagueId == leagueId).Select(c => new Roster()
+                {
+                    RosterId = c.RosterId,
+                    RosterName = c.RosterName,
+                    GameDate = c.GameDate,
+                    LeagueId = c.League.LeagueId,
+                    RuleSetsUsedEnum = c.RuleSetsUsedEnum,
+                    RosterSize = c.RosterMembers.Count()
+                }).ToList();
+
+
+                return rosters;
+            }
+            catch (Exception exception)
+            {
+                ErrorDatabaseManager.AddException(exception, exception.GetType());
+            }
+            return null;
+        }
+
+        public static Roster GetLeagueRoster(long linkId, Guid leagueId)
+        {
+            List<Roster> itemRosters = new List<Roster>();
+            try
+            {
+                var dc = new ManagementContext();
+
+                var roster = dc.Rosters.FirstOrDefault(x => x.League.LeagueId == leagueId);
+                var firstOrDefault = roster.RosterMembers.FirstOrDefault();
+                if (firstOrDefault != null)
+                {
+                    var item = new Roster()
+                    {
+                        RosterId = roster.RosterId,
+                        RosterName = roster.RosterName,
+                        GameDate = roster.GameDate,
+                        LeagueId = roster.League.LeagueId,
+                        RuleSetsUsedEnum = roster.RuleSetsUsedEnum,
+                        InsuranceTypeId = firstOrDefault.InsuranceType,
+                        RosterSize = roster.RosterMembers.Count()
+                    };
+                    var rosterMembers = roster.RosterMembers.Select(x => new KeyValueHelper()
+                    {
+                        Id = x.Member.MemberId,
+                        Name = x.Member.Name
+                    }).ToList();
+
+                    item.RosterMembers = rosterMembers;
+                    return item;
+                }
+            }
+            catch (Exception exception)
+            {
+                ErrorDatabaseManager.AddException(exception, exception.GetType());
+            }
+            return null;
+        }
+
+        public static bool UpdateRoster(Roster roster)
+        {
+            try
+            {
+
+                var dc = new ManagementContext();
+                var rosterObj = dc.Rosters.Where(x => x.RosterId == roster.RosterId).FirstOrDefault();
+                if (rosterObj == null)
+                    return false;
+                rosterObj.RosterName = roster.RosterName;
+                rosterObj.GameDate = roster.GameDate;
+                rosterObj.League = dc.Leagues.FirstOrDefault(x => x.LeagueId == roster.LeagueId);
+                rosterObj.RuleSetsUsedEnum = roster.RuleSetsUsedEnum;
+                var rosterMemberIds = roster.RosterMemberIds.Split(',').ToList().ConvertAll(Guid.Parse);
+                var rosterMembersToRemove = rosterObj.RosterMembers.Where(x => !rosterMemberIds.Contains(x.Member.MemberId));
+                var existingrosterMembers = rosterObj.RosterMembers.Where(x => rosterMemberIds.Contains(x.Member.MemberId));
+                dc.RosterMembers.RemoveRange(rosterMembersToRemove);
+                existingrosterMembers.ForEach(x => x.InsuranceType = roster.InsuranceTypeId);
+                foreach (var memId in rosterMemberIds)
+                {
+                    if (!rosterObj.RosterMembers.Any(x => x.Member.MemberId == memId))
+                    {
+                        var rosterMember = new RosterMember();
+                        rosterMember.Member = dc.Members.FirstOrDefault(x => x.MemberId == memId);
+                        rosterMember.Roster = rosterObj;
+                        rosterMember.InsuranceType = roster.InsuranceTypeId;
+                        rosterObj.RosterMembers.Add(rosterMember);
+                    }
+                }
+                int c = dc.SaveChanges();
+                return c > 0;
+            }
+            catch (Exception exception)
+            {
+                ErrorDatabaseManager.AddException(exception, exception.GetType());
+            }
+            return false;
+        }
+    }
+}
